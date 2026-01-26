f7192d2a2c9d4f842aa9474b53a6d8bfe1e81598
Jerryyy <nguyendhse171946@fpt.edu.vn>
2026-01-26 14:25:24 +0700

    new file:   .gitignore 	new file:   .prettierignore 	new file:   .prettierrc.json 	new file:   LICENSE 	new file:   assets/img/aws-white.png 	new file:   assets/img/favicon.ico 	new file:   assets/img/logo.png 	new file:   assets/img/meta.jpg 	new file:   assets/img/systems-data-center.png 	new file:   assets/js/challenges.js 	new file:   assets/js/color_mode_switcher.js 	new file:   assets/js/components/language.js 	new file:   assets/js/index.js 	new file:   assets/js/notifications.js 	new file:   assets/js/page.js 	new file:   assets/js/scoreboard.js 	new file:   assets/js/settings.js 	new file:   assets/js/setup.js 	new file:   assets/js/teams/list.js 	new file:   assets/js/teams/private.js 	new file:   assets/js/teams/public.js 	new file:   assets/js/theme/highlight.js 	new file:   assets/js/theme/styles.js 	new file:   assets/js/theme/times.js 	new file:   assets/js/users/list.js 	new file:   assets/js/users/private.js 	new file:   assets/js/users/public.js 	new file:   assets/js/utils/alerts.js 	new file:   assets/js/utils/clipboard.js 	new file:   assets/js/utils/collapse.js 	new file:   assets/js/utils/graphs/echarts/categories.js 	new file:   assets/js/utils/graphs/echarts/index.js 	new file:   assets/js/utils/graphs/echarts/scoreboard.js 	new file:   assets/js/utils/graphs/echarts/solve-percentage.js 	new file:   assets/js/utils/graphs/echarts/userscore.js 	new file:   assets/js/utils/math.js 	new file:   assets/js/utils/notifications/alerts.js 	new file:   assets/js/utils/notifications/read.js new file:   assets/js/utils/notifications/toasts.js 	new file:   assets/js/utils/objects.js 	new file:   assets/js/utils/tooltips.js 	new file:   assets/scss/includes/components/_challenge.scss 	new file:   assets/scss/includes/components/_graphs.scss 	new file:   assets/scss/includes/components/_jumbotron.scss 	new file:   assets/scss/includes/components/_scoreboard.scss 	new file:   assets/scss/includes/components/_sticky-footer.scss 	new file:   assets/scss/includes/components/_table.scss 	new file:   assets/scss/includes/icons/_award-icons.scss 	new file:   assets/scss/includes/icons/_flag-icons.scss 	new file:   assets/scss/includes/utils/_cursors.scss 	new file:   assets/scss/includes/utils/_fonts.scss 	new file:   assets/scss/includes/utils/_lolight.scss 	new file:   assets/scss/includes/utils/_min-height.scss 	new file:   assets/scss/includes/utils/_opacity.scss new file:   assets/scss/main.scss 	new file:   package.json 	new file:   postcss.config.js 	new file:   static/manifest.json 	new file:   templates/base.html new file:   templates/challenge.html 	new file:   templates/challenges.html 	new file:   templates/components/errors.html 	new file:   templates/components/navbar.html 	new file:   templates/components/notifications.html 	new file:   templates/config.html 	new file:   templates/confirm.html 	new file:   templates/errors/403.html 	new file:   templates/errors/404.html 	new file:   templates/errors/429.html 	new file:   templates/errors/500.html 	new file:   templates/errors/502.html 	new file:   templates/login.html 	new file:   templates/macros/forms.html 	new file:   templates/notifications.html 	new file:   templates/page.html 	new file:   templates/register.html 	new file:   templates/reset_password.html 	new file:   templates/scoreboard.html 	new file:   templates/settings.html 	new file:   templates/setup.html 	new file:   templates/teams/invite.html 	new file:   templates/teams/join_team.html 	new file:   templates/teams/new_team.html new file:   templates/teams/private.html 	new file:   templates/teams/public.html 	new file:   templates/teams/team_enrollment.html 	new file:   templates/teams/teams.html 	new file:   templates/users/private.html 	new file:   templates/users/public.html 	new file:   templates/users/users.html 	new file:   vite.config.js

---
+++ assets/js/color_mode_switcher.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function getPreferredTheme() {
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    return storedTheme;
+  }
+  return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
+
+function showActiveTheme(theme) {
+  const activeThemeIcon = document.querySelector(".theme-switch i.fas");
+  activeThemeIcon.classList.toggle("fa-moon", theme === "dark");
+  activeThemeIcon.classList.toggle("fa-sun", theme !== "dark");
+}
+
+let currentTheme = getPreferredTheme();
+document.documentElement.setAttribute("data-bs-theme", currentTheme);
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+  currentTheme = getPreferredTheme();
+  document.documentElement.setAttribute("data-bs-theme", currentTheme);
+  showActiveTheme(currentTheme);
+});
+
+window.addEventListener("load", () => {
+  showActiveTheme(currentTheme);
+
+  document.querySelectorAll(".theme-switch").forEach(e => {
+    e.addEventListener("click", ev => {
+      currentTheme = currentTheme === "light" ? "dark" : "light";
+      document.documentElement.setAttribute("data-bs-theme", currentTheme);
+      localStorage.setItem("theme", currentTheme);
+      showActiveTheme(currentTheme);
+      ev.preventDefault();
+    });
+  });
+});
